5e91c9e492b2ec2226bbc6588380c81f876acc1f
Everton Porto <everton.porto@outlook.com>
2022-08-11 13:47:07 -0500

    BasketBall Score Board

---
+++ index.js
@@ -0,0 +1,35 @@
+let pointsEl = document.getElementById("points-el");
+let gpointsEl = document.getElementById("gpoints-el");
+let hScore = 0
+let gScore = 0
+
+function add1H() {
+    hScore = hScore + 1;
+    pointsEl.innerHTML = hScore
+}
+
+function add2H() {
+    hScore = hScore + 2;
+    pointsEl.innerHTML = hScore
+}
+
+function add3H() {
+    hScore = hScore + 3;
+    pointsEl.innerHTML = hScore
+}
+
+function add1G() {
+    gScore = gScore + 1;
+    gpointsEl.innerHTML = gScore
+}
+
+function add2G() {
+    gScore = gScore + 2;
+    gpointsEl.innerHTML = gScore
+}
+
+function add3G() {
+    gScore = gScore + 3;
+    gpointsEl.innerHTML = gScore
+}
+
